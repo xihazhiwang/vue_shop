cecfa5c7b4e86e33cff6ab3ee57114bc54543225
314893404@qq.com <Github123>
2020-01-08 23:25:20 +0800

    完成了登录功能

--- src/router.js
+++ src/router.js
@@ -1,16 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from './components/Login.vue'
+import Home from './components/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: '/login' },
-  { path: '/login', component: Login }
+  { path: '/login', component: Login },
+  { path: '/home', component: Home }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+// 挂在导航守卫
+router.beforeEach((to, from, next) => {
+  // to将要访问的页面
+  // from代表从哪个路径而来
+  // next 是一个函数，表示放行 next()   next('/login')强制跳转
+  if (to.path === '/login') {
+    return next()
+  } else {
+    const tokenStr = window.sessionStorage.getItem('token')
+    if (tokenStr) {
+      next()
+    } else {
+      return next('/login')
+    }
+  }
 })
 
 export default router
