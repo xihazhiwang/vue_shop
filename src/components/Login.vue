6dfc026237d001df2416795169b3ff5cfa5b2b9c
314893404@qq.com <Github123>
2020-01-08 23:24:33 +0800

    完成了登录功能

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,16 +6,18 @@
         <img src="../assets/logo.png"/>
       </div>
       <!-- 登录表单 -->
-      <el-form label-width="0px" class="login_form">
-        <el-form-item>
-          <el-input></el-input>
+      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input></el-input>
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" prefix-icon="iconfont icon-mima"
+                    type="password"
+          ></el-input>
         </el-form-item>
         <el-form-item class="btns">
-          <el-button type="primary">登录</el-button>
-          <el-button type="info">重重</el-button>
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重重</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -24,7 +26,49 @@
 
 <script>
 export default {
-  name: 'Login'
+  name: 'Login',
+  data () {
+    return {
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      // 表单的验证规则
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在3到10个之间', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在6到15个之间', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    // 充值登录表单
+    resetLoginForm () {
+      this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate(async valid => {
+        console.log(valid)
+        if (valid) {
+          const { data: res } = await this.$http.post('login', this.loginForm)
+          if (res.meta.status === 200) {
+            this.$message.success('登录成功')
+            console.log(res)
+            console.log(res.data.token)
+            window.sessionStorage.setItem('token', res.data.token)
+            this.$router.push('/home')
+          } else {
+            this.$message.error('登录失败')
+          }
+        }
+      })
+    }
+  }
 }
 </script>
 
