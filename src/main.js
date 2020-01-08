f8375d7f29224078bbc9a59b050f9d988bb2e636
314893404@qq.com <GitHub123>
2020-01-08 13:57:10 +0800

    第一天提交

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+/**
+ * 全局样式表
+ */
+import './assets/css/global.css'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 new Vue({
   router,
