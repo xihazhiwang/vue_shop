9189b612776cce8de2a26df744ec62c929faf51d
314893404@qq.com <Github123>
2020-01-08 23:40:37 +0800

    去掉eslint的校验功能

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,33 @@
 <template>
-<div>
-  <el-button type="info" @click="logout">退出</el-button>
-</div>
+  <!--头部区域-->
+  <el-container>
+    <el-header>Header
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-  name: 'Login',
-  data () {
-    return {}
-  },
-  methods: {
-    logout () {
-      window.sessionStorage.clear()
-      this.$router.push('/login')
+  export default {
+    name: 'Login',
+    data() {
+      return {}
+    },
+    methods: {
+      logout() {
+        window.sessionStorage.clear()
+        this.$router.push('/login')
+      }
     }
   }
-}
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+  .el-header {
+
+  }
+</style>
